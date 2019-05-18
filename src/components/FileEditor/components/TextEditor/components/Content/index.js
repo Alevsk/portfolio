488590ac2b97c136802f2a57ac8cfdfbfc56b9c8
Lenin Alevski Huerta Arias <alevsk.8772@gmail.com>
2019-05-18 02:00:25 -0500

    adding coding highlight component

--- src/components/FileEditor/components/TextEditor/components/Content/index.js
+++ src/components/FileEditor/components/TextEditor/components/Content/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atomOneDark } from 'react-syntax-highlighter/dist/styles/hljs'
+
 import './styles.css';
 
 function Content(props) {
   return (
-    <div className="Content">
+    <SyntaxHighlighter
+      language='javascript'
+      style={atomOneDark}
+      showLineNumbers
+      className="Content"
+      lineNumberContainerStyle={{
+        float: 'left',
+        paddingRight: '15px',
+        paddingLeft: '10px',
+        textAlign: 'right',
+        color: '#4D5059',
+      }}
+    >
       {props.content}
-    </div>
+    </SyntaxHighlighter>
   );
 }
 
--- src/components/FileEditor/components/TextEditor/index.js
+++ src/components/FileEditor/components/TextEditor/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import LineNumber from './components/LineNumber';
 import Content from './components/Content';
 import './styles.css';
 
 function TextEditor(props) {
   const file = props.selectedFile || { content: '' };
-  const lines = (file.content.match(/\n/g) || '').length + 1
   return (
     <div className="TextEditor">
-      <LineNumber lines={lines}/>
       <Content content={file.content}/>
     </div>
   );
